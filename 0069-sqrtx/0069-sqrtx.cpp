8bb6d8582eede417d3cb9d9f78fbff962c95bf20
Tanay Gupta <130499130+tanay28gupta@users.noreply.github.com>
2025-08-18 15:12:50 +0530

    Time: 20 ms (5.63%), Space: 8.4 MB (85.01%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -5,16 +5,16 @@ public:
         {
             return x;
         }
-        //int i=1;
-        // while(i*i<=x)
-        // {
-        //     i++;
-        // }
-        long long i;
-        for ( i=1;i*i <=x ;i++)
-        {
-
+      long long i;
+         while(i*i<=x)
+         {
+             i++;
         }
+        
+        // for ( i=1;i*i <=x ;i++)
+        // {
+
+        // }
         return i-1;
     }
 };
