88f51c7c097423d086edb4c484c12817b06c6069
Tanay Gupta <130499130+tanay28gupta@users.noreply.github.com>
2025-08-18 11:26:41 +0530

    Time: 0 ms (100%), Space: 11.7 MB (94.93%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        if (strs.empty()) return "";          // (A)
-
-        string prefix = "";                    // (B)
-        string first = strs[0];                // (C)
-
-        // (D) i = character index in the first word
-        for (int i = 0; i < first.size(); i++) {
-            char c = first[i];                 // (E) character to check
-
-            // (F) j = which word we're checking (start from 1, skip the first)
-            for (int j = 1; j < strs.size(); j++) {
-                // (G) out of range or mismatch => stop and return current prefix
-                if (i >= strs[j].size() || strs[j][i] != c) {
-                    return prefix;
-                }
-            }
-
-            // (H) if every word had the same char at position i, add it
-            prefix += c;
+       if(strs.empty()) return "";
+       
+       string prefix ="";
+       string first=strs[0];
+       for(int i=0;i<first.size();i++)
+       {
+        char c=first[i];
+    
+       // now well check it with the other words the forst character of the word and if they're same then we'll store it in the the prefix variable that we have created above
+       for(int j=1;j<strs.size();j++)
+       {
+        if(i>strs[j].size() || strs[j][i] != c)
+        {
+            return prefix;
         }
+       }
+       prefix +=c;
 
-        return prefix;                         // (I)
+       }
+return prefix;
     }
 };
+
+
+
+
+//  make the size of the word store in the variable
+// then after storing the size make the indexes compare line by line what ever is same between all the words make them store in the length of the variabla then return the variable to get the result 
