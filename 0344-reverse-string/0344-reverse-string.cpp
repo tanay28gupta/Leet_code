fe3c0c7102f6cb979c25576e788c3235048355f7
Tanay Gupta <130499130+tanay28gupta@users.noreply.github.com>
2025-09-04 18:29:48 +0530

    Time: 0 ms (100%), Space: 27.2 MB (79.9%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int right =s.size()-1;
+        int left =0;
+
+        while (left<right)
+        {
+            swap(s[left],s[right]);
+            left++;
+            right--;
+        }
+    }
+};
