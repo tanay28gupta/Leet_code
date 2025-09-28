9b10f0628e93028d9a03d7ae2e503b4b0d7ff544
Tanay Gupta <130499130+tanay28gupta@users.noreply.github.com>
2025-09-29 01:01:37 +0530

    Time: 3 ms (31.95%), Space: 10.8 MB (6.16%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,20 +2,24 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         //CONVERTING ALL THE LETTERS INTO LOWER CASE LETETRS AND THEN removing any removing any non alphanumeric leters and numbers and then it should read same forward and backwords.
-        string cleaned="";
-        for (size_t i=0;i<s.size();++i){
-            char c=s[i];
-            if (isalnum(c)){
-                cleaned += tolower(c);
-            }
+    int n=s.size();
+    string cleaned="";
+    for (int i=0;i<n;i++)
+    {
+        char c=s[i];
+        if(isalnum(c))
+        {
+            cleaned+=tolower(c);
         }
-
+    }
         string reversed =cleaned;
         reverse(reversed.begin(),reversed.end());
-        
-        return cleaned==reversed;
-
-
-        
+        if(cleaned==reversed)
+        {
+            return true;
+        }
+   
+    
+             return false;
     }
 };
